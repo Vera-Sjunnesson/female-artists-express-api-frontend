2484d601a86e7ee3408e07b08ab57e7e245d2b9e
Vera Sjunnesson <vera.kastrup@gmail.com>
2023-11-15 22:25:03 +0100

    added component for artist details

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { StarterPage } from './components/StarterPage'; 
 import { AllArtists } from './components/AllArtists';
+import { ArtistDetails } from './components/ArtistDetails';
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<StarterPage />} />
         <Route path="/artists" element={<AllArtists />} />
+        <Route path="/artists/:id" element={<ArtistDetails />} />
       </Routes>
     </BrowserRouter>
   )
