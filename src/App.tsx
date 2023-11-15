ba56eac99f663b52b732baa11441db81cd258a5d
Vera Sjunnesson <vera.kastrup@gmail.com>
2023-11-15 21:29:44 +0100

    added one more component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
 import { StarterPage } from './components/StarterPage'; 
 import { AllArtists } from './components/AllArtists';
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<StarterPage />} />
         <Route path="/artists" element={<AllArtists />} />
